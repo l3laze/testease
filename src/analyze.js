48e9b1a62afadd007c0dda202c2d917519441c0e
Tom Shaver <l3l_aze@yahoo.com>
2021-05-04 18:23:35 -0500

    Colors, percents
    * Feat: Partially color error messages.
    * Feat: Show pass/fail/error % in results.

--- src/analyze.js
+++ src/analyze.js
@@ -2,14 +2,18 @@
 
 const { format, green, red, reset, yellow } = require('./common.js')
 
+function percentage (dividend, divisor) {
+  return dividend !== 0 ? (dividend / divisor * 100) : 0
+}
+
 function analyze () {
   let message
 
-  message = format('\n%spass%s    %d/%d %i%', green, reset, this.testsPassed, this.testsTotal, this.testsPassed !== 0 ? (this.testsPassed / this.testsTotal * 100) : 0)
+  message = format('\n%spass%s    %d/%d %i%', green, reset, this.testsPassed, this.testsTotal, percentage(this.testsPassed, this.testsTotal))
 
-    message += format('\n%sfail%s    %d/%d %i%', yellow, reset, this.testsFailed, this.testsTotal,  this.testsFailed !== 0 ? (this.testsPassed / this.testsTotal * 100) : 0)
+    message += format('\n%sfail%s    %d/%d %i%', yellow, reset, this.testsFailed, this.testsTotal,  percentage(this.testsFailed, this.testsTotal))
 
-    message += format('\n%serror%s   %d/%d %i%', red, reset, this.testsError, this.testsTotal, this.testsError !== 0 ? (this.testsPassed / this.testsTotal * 100) : 0)
+    message += format('\n%serror%s   %d/%d %i%', red, reset, this.testsError, this.testsTotal, percentage(this.testsError, this.testsTotal))
 
   console.info(message + '\n')
 
--- src/printResult.js
+++ src/printResult.js
@@ -1,13 +1,21 @@
 'use strict'
 
-const { format, passed, failed, error } = require('./common.js')
+const { format, passed, failed, error, red, reset } = require('./common.js')
+
+function colorErrors (err) {
+  const formatted = err.replace(/(at (?:[^(])+)/g, format('%s%s%s', red, '$1', reset))
+
+  return formatted
+}
 
 function printResult () {
   const keys = Object.keys(this.tests)
-  let message, diff
+  let message, diff, i
 
-  for (let i = 0; i < keys.length; i++) {
+  for (i = 0; i < keys.length; i++) {
     diff = this.tests[keys[i]].end - this.tests[keys[i]].start
+
+    // console.info(i, keys[i], keys)
 
     if (this.tests[keys[i]].phase === 'describe') {
       message = format('\n%s +%dms', keys[i], diff)
@@ -16,11 +24,13 @@ function printResult () {
       message = format('%s %s +%dms', (this.tests[keys[i]].result === true ? passed : failed), keys[i], diff)
     }
     else if (typeof this.tests[keys[i]].error !== 'undefined') {
-      message = format('%s %s +%dms\n%s', error, keys [i], diff, this.tests[keys[i]].error.stack)
+      message = format('%s %s +%dms\n%s', error, keys [i], diff, colorErrors(this.tests[keys[i]].error.stack))
     }
 
     console.info(message)
   }
+
+  this.tests = {}
 }
 
 module.exports = {
