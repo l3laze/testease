1a43ece431d2b5b8933e7a2e5a63b126d6897f56
Tom Shaver <l3l_aze@yahoo.com>
2024-02-17 20:36:40 -0600

    Cleanup
    * Remove excessive tests.
    * Shorten/clarify test labels.
    * Separate awaiting reporter results and returning them.

--- self-exam.js
+++ self-exam.js
@@ -6,23 +6,23 @@ import { benchmark } from './bench.js'
 async function selfExam () {
   describe('Testease Framework')
 
-  it('allows synchronous success', function () {
+  it('synchronous success', function () {
     return true
   })
 
-  it.fails('allows synchronous failure', function () {
-    return false
-  })
-
-  await it('allows async success', async function () {
+  await it('async success', async function () {
     return true
   })
 
-  await it.fails('allows async failure', async function () {
+  it.fails('synchronous failure', function () {
     return false
   })
 
-  await it.fails('allows timeout of async tests', async function () {
+  await it.fails('async failure', async function () {
+    return false
+  })
+
+  await it.fails('timeout of async tests', async function () {
     await new Promise((resolve) => setTimeout(resolve, 100))
 
     return true
@@ -36,36 +36,8 @@ async function selfExam () {
     throw new Error('Hello, error!')
   })
 
-  it.fails('errors on non-boolean result - Number', function () {
+  it.fails('non-boolean as test result causes failure', function () {
     return 0
-  })
-
-  it.fails('errors on non-boolean result - String', function () {
-    return 'Hello'
-  })
-
-  it.fails('errors on non-boolean result - Object', function () {
-    return {
-      result: true
-    }
-  })
-
-  it.fails('errors on non-boolean result - Array', function () {
-    return [true]
-  })
-
-  it.fails('errors on non-boolean result - Symbol', function () {
-    return Symbol('Oops')
-  })
-
-  it.fails('errors on non-boolean result - function', function () {
-    const dummy = function dummy () {}
-    dummy()
-    return dummy
-  })
-
-  it.fails('errors on non-boolean result - Promise', function () {
-    return (async () => 'World!')()
   })
 
   it.fails('errors on attempted timeout of sync code', function () {
@@ -75,7 +47,9 @@ async function selfExam () {
     throw new Error('Oops')
   })
 
-  return await reporter()
+  const results = await reporter()
+
+  return results
 }
 
 const args = process?.argv.slice(2).map((a) => a.toLowerCase())
